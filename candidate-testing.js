d169f8e8545f672dd864acc925f6a64e4978c443
trose1237 <85703591+trose1237@users.noreply.github.com>
2021-06-28 19:32:57 +0000

    Part 3 finished

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,44 +10,59 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = " ";
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question("Enter Name: ")
+  candidateName = input.question("Enter Name: ")
 }
 
-//function askQuestion() {
+function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- //candidateAnswers = input.question(questions)
- 
-for (let i = 0; i < questions.length; i++){
-  console.log(questions[i])
-  
-}
 
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]))
+console.log(`Your Answer:  ${candidateAnswers[i]}
+Correct Answer: ${correctAnswers[i]}`)
+console.log("\n")
+  }
+}
 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer){
-  console.log("Correct answer!")
-} else {
-  console.log("Incorrect answer.")
-}
-  let grade;
-  
+let grade = 0;
 
-  return grade;
+ 
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i] == correctAnswers[i]) {
+      grade++;
+      
+    }
+    
+
+  }
+let candidatePercentage = (grade / 5 * 100);
+
+  if (candidatePercentage >= 80) {
+    console.log(">>> Overall Grade: " + candidatePercentage + "% (" + grade + " of 5 responses correct) <<<")
+    console.log(">>> Status: PASSED <<<")
+  } else {
+    console.log(">>> Overall Grade: " + candidatePercentage + "% (" + grade + " of 5 responses correct) <<<")
+    console.log(">>> Status: FAILED <<<")
+  }
+  
+    return grade;
+  
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
 
-  console.log("Hello " + candidateName + "," )
+  console.log("Candidate Name: " + candidateName)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
